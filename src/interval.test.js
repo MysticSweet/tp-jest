2d6825bbed1f55628c34e6bae52dd12a1e9ae2dd
Yoan <yoanboit@hotmail.fr>
2019-10-07 13:30:15 +0200

    ajout de la fonction 'includes' et tests associés

--- src/interval.test.js
+++ src/interval.test.js
@@ -23,3 +23,32 @@ describe('overlaps', function () {
         expect(interval1.overlaps(intervalI)).toBe(false)
     });
 });
+
+describe('includes', function () {
+    var interval1 = new Interval(1,5);
+    var interval1Bis = new Interval(1,5);
+    var interval2 = new Interval(2,3);
+    var interval3 = new Interval(0,6);
+    var intervalLeft = new Interval(0,4);
+    var intervalRight = new Interval(2,7);
+    var intervalOut = new Interval(50,66);
+
+    test('Test includes - intervals identiques => true', () => { 
+        expect(interval1.includes(interval1Bis)).toBe(true)
+    });
+    test('Test includes - interval2 dans interval1 => true', () => { 
+        expect(interval1.includes(interval2)).toBe(true)
+    });
+    test('Test includes - interval3 plus grand que interval1 => false', () => { 
+        expect(interval1.includes(interval3)).toBe(false)
+    });
+    test('Test includes - interval plus grand (limite gauche) => false', () => { 
+        expect(interval1.includes(intervalLeft)).toBe(false)
+    });
+    test('Test includes - interval plus grand (limite droite) => false', () => { 
+        expect(interval1.includes(intervalRight)).toBe(false)
+    });
+    test('Test includes - intervals séparés => false', () => { 
+        expect(interval1.includes(intervalOut)).toBe(false)
+    });
+});
